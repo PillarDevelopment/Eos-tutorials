3a22b395380a0279ba769c819ef9b0067446a40f
PillarDevelopment <2622610@gmail.com>
2018-06-18 22:15:11 +0300

    Контракт обменника(исходный код)

--- currency.contract/currncy.cpp
+++ currency.contract/currncy.cpp
@@ -2,3 +2,27 @@
 // Created by Ivan Borisov on 18.06.2018.
 //
 
+#include <currency/currency.hpp> /// defines transfer struct (abi)
+namespace TOKEN_NAME {
+/// При хранении учетных записей проверяется
+/// наличие пустого баланса и удаляется аккаунт.
+void  storeAccount(AccountName account, const Account& a) {
+    if(a.isEmpty()) {
+        printi(account);
+        /// scope table key
+        Db::store(account, N(account), N(account), a);
+    }
+}
+    void  apply_currency_transfer(const TOKEN_NAME::Transfer&transfer) {
+    requireNotice(transfer.to, transfer.from);
+    requireAuth(transfer.from);
+    auto from = getAccount(transfer.from);
+    auto to = getAccount(transfer.to);
+    /// вычитание токена может вызвать обратное переполнение
+    from.balance -= transfer.quality;
+    /// добавление токена может вызвать переполнение
+    to.balance += transfer.quality;
+    storeAccount(transfer.from, from);
+    storeAccount(transfer.to, to);
+    }
+} // namespace TOKEN_Name
