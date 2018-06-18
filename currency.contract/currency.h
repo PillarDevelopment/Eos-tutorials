ce2a464811d7e97e0d30a9b5dcc18923c7f607fe
PillarDevelopment <2622610@gmail.com>
2018-06-18 18:13:11 +0300

    create new contract

---
+++ currency.contract/currency.h
@@ -0,0 +1,8 @@
+//
+// Created by Ivan Borisov on 18.06.2018.
+//
+
+#ifndef EOS_TUTORIALS_CURRENCY_H
+#define EOS_TUTORIALS_CURRENCY_H
+
+#endif //EOS_TUTORIALS_CURRENCY_H
---
+++ currency.contract/currncy.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Ivan Borisov on 18.06.2018.
+//
+
