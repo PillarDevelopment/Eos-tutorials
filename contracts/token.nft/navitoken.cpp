fc1e9bbe84cabbca3f6b52464aac96c682b27c41
Ivan Borisov <2622610@gmail.com>
2018-07-19 19:03:49 +0300

    добавил словарь naviaddrssstorage и несколько комментариев

--- contracts/token.nft/navitoken.cpp
+++ contracts/token.nft/navitoken.cpp
@@ -1,19 +1,24 @@
 //
 // Created by Ivan Borisov on 19.07.2018.
 //
-#include<iostream>
+#include <iostream>
+#include <map>
+//#include <vector>
 #include "navitoken.h"
 
 using namespace std;
 
 int main() {
 
-    struct addressInfo {
+    struct addressInfo { // создал пользовательский тип для хранения метаданных по адресу
         typedef uint64_t  token_id;
         typedef atomic_char16_t  ipfs_hash; // пока взял как замену bytes
         typedef uint8_t ipfs_hash_function;
         typedef uint8_t ipfs_size;
     };
+
+    map <atomic_char16_t, addressInfo> naviAddressStorage; // создадим словарь(меппинг) для хранения метадаты по хешу
+
 }
 
 
