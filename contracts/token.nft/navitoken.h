cc976112e830bffb8eb0003b20018594b5c94c85
Ivan Borisov <2622610@gmail.com>
2018-07-22 23:23:10 +0300

    добавлен импорт в заголовок

--- contracts/token.nft/navitoken.h
+++ contracts/token.nft/navitoken.h
@@ -2,8 +2,13 @@
 // Created by Ivan Borisov on 19.07.2018.
 //
 
+#include <eosiolib/asset.hpp>
+#include <eosiolib/eosio.hpp>
+
+#include <string>
 #ifndef COURSERA_YANDEX_NAVITOKEN_H
 #define COURSERA_YANDEX_NAVITOKEN_H
+
 
 
 class navitoken {
