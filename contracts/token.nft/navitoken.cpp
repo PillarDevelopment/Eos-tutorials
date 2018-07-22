c3bdbdd8d41c96793ca17e21a474843b96b0202c
Ivan Borisov <2622610@gmail.com>
2018-07-22 23:07:32 +0300

    Update navitoken.cpp

--- contracts/token.nft/navitoken.cpp
+++ contracts/token.nft/navitoken.cpp
@@ -8,22 +8,25 @@
 
 using namespace std;
 
-int main() {
-    
-    string name;
+
+string name;
     string symbol;
     string totalSupply;
-    
-    
-    struct addressInfo { // создал пользовательский тип для хранения метаданных по адресу
+
+struct addressInfo { // создал пользовательский тип для хранения метаданных по адресу
         typedef uint64_t  token_id;
         typedef atomic_char16_t  ipfs_hash; // пока взял как замену bytes
         typedef uint8_t ipfs_hash_function;
         typedef uint8_t ipfs_size;
     };
 
-    map <atomic_char16_t, addressInfo> naviAddressStorage; // создадим словарь(меппинг) для хранения метадаты по хешу
+map <atomic_char16_t, addressInfo> naviAddressStorage; // создадим словарь(меппинг) для хранения метадаты по хешу
 
+int balanceOf() public {
+} 
+    
+int main() {
+    return 0;
 }
 
 // методы
